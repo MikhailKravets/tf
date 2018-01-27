8229e71829c9f819f5f54f1bab6e161e4f9c27b4
Mikhail Kravets <22kravets22@mail.ru>
2018-01-27 22:03:55 +0200

    Broke regression

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -4,32 +4,41 @@ import tqdm
 import matplotlib.pyplot as pl
 
 
-x = np.linspace(-1, 2, 1000)
-y = np.sin(x) * np.e ** -x + np.random.normal(size=len(x))
+def transform(x, power):
 
-alpha = 0.9
+    res = np.hstack((np.ones(shape=(len(x), 1)), x))
+    for p in range(2, power + 1):
+        res = np.hstack((res, x ** p))
+    return res
+
+
+alpha = 0.2
+order = 4
+epochs = 4000
+
+x = np.linspace(-1, 2, 1000)
+y = np.sin(x) * np.e ** -x
+
+x_train = transform(x.reshape((len(x), 1)), order)
+y_train = y.reshape((len(y), 1))
 
 with tf.Session() as sess:
-    x_ = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='input')
-    y_ = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='output')
+    x_ = tf.placeholder(dtype=tf.float32, shape=(None, order + 1), name='input')
+    y_ = tf.placeholder(dtype=tf.float32, shape=(None, 1), name='output')
 
-    bias = tf.Variable(tf.random_normal([1]), name='bias')
-    w1 = tf.Variable(tf.random_normal([1]))
-    w2 = tf.Variable(tf.random_normal([1]))
-    w3 = tf.Variable(tf.random_normal([1]))
+    w = tf.Variable(tf.random_normal([order + 1, 1]), name='w')
 
-    model_output = bias + w1 * x_ + w2 * tf.pow(x_, 2) + w3 * tf.pow(x_, 3)
-    loss = tf.reduce_mean(tf.pow(y_ - model_output, 2)) + alpha * tf.nn.l2_loss(w1 + w2 + bias)
+    model_output = tf.matmul(x_, w)
+    loss = tf.reduce_mean((y_ - model_output) ** 2) + alpha * tf.nn.l2_loss(w)
 
     gd = tf.train.GradientDescentOptimizer(0.01)
 
     train_step = gd.minimize(loss)
     sess.run(tf.global_variables_initializer())
 
-    epochs = 1000
     for i in tqdm.tqdm(range(epochs)):
-        unk, err = sess.run([train_step, loss], feed_dict={x_: x.reshape((len(x), 1)), y_: y.reshape((len(y), 1))})
+        unk, err = sess.run([train_step, loss], feed_dict={x_: x_train, y_: y_train})
 
     pl.plot(x, y)
-    pl.plot(x, sess.run(model_output, feed_dict={x_: x.reshape((len(x), 1))}))
+    pl.plot(x_train[:,1], sess.run(model_output, feed_dict={x_: x_train}))
     pl.show()
